cc02fe569aae6b97c2054a05e52c8dcb73cacfc8
Code04-Miraj <mirajkattel7@gmail.com>
2025-08-31 11:40:46 +0545

    Step 2: Create Post model and admin setup

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (('draft','Draft'), ('published' , 'Published'))
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True,blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+
+    def save(self, *args , **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+        super().save(*args , **kwargs)
+
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.slug])
+
+
+    def __str__(self):
+        return self.title        
+
+
+
+
